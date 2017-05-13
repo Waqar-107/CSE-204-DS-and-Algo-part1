865810d4c1ad4114d34ff426f415e622547ba596
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2017-05-13 17:13:47 +0700

    Update Inversion.cpp

--- week-11/Inversion.cpp
+++ week-11/Inversion.cpp
@@ -1,74 +1,75 @@
-#include<iostream>
-using namespace std;
+#include<cstdio>
 
-int Merge(int a[],int l,int m,int r,int count)
+int Merge(int a[],int l,int m,int r)
 {
     int i,j,k;
-    int n1=m-l+1;
-    int n2=r-m;
+    int n1,n2;
     int local_count=0;
 
-    int L[n1],R[n2];
+    n1=m-l+1;
+    n2=r-m;
 
+    //x and y for left and right sub-array
+    int x[n1],y[n2];
+
+    //---------------------------------copying the sub-arrays
     k=l;
     for(i=0;i<n1;i++)
-    {
-        L[i]=a[k];
-        k++;
-    }
-
+        x[i]=a[k],k++;
 
     k=m+1;
     for(i=0;i<n2;i++)
-    {
-        R[i]=a[k];
-        k++;
-    }
+        y[i]=a[k],k++;
+    //---------------------------------copying the sub-arrays
 
-    //merge into one array
-    //counting inversions
-    i=0;j=0;k=l;
+
+    //---------------------------------merging the sub-arrays
+    k=l;
+    i=0,j=0;
     while(i<n1 && j<n2)
     {
-        //elements placed in right order
-        if(L[i]<=R[j])
+        if(x[i]<=y[j])
         {
-            a[k]=L[i];i++;
+            a[k]=x[i];
+            i++;
         }
 
         else
         {
-            a[k]=R[j];j++;
+            a[k]=y[j];
+            j++;
+
             local_count+=(n1-i);
         }
 
         k++;
     }
+    //---------------------------------merging the sub-arrays
 
-    while(i<n1)
-    {
-        a[k]=L[i];i++;k++;
-    }
 
-    while(j<n2)
-    {
-        a[k]=R[j];j++;k++;
-    }
+    //one of the array will be empty but the other won't
+    //---------------------------------copying the rest of the items
+    for(int w=i;w<n1;w++)
+        a[k]=x[w],k++;
 
-    return count+local_count;
+    for(int w=j;w<n2;w++)
+        a[k]=y[w],k++;
+    //---------------------------------copying the rest of the items
+
+    return local_count;
 }
 
-int MergeSort(int a[],int l,int r,int count)
+int MergeSort(int a[],int l,int r)
 {
     int x=0;
     if(l<r)
     {
         int m=(l+r)/2;
 
-        x+=MergeSort(a,l,m,count);
-        x+=MergeSort(a,m+1,r,count);
+        x+=MergeSort(a,l,m);
+        x+=MergeSort(a,m+1,r);
 
-        x+=Merge(a,l,m,r,count);
+        x+=Merge(a,l,m,r);
     }
 
     return x;
@@ -76,22 +77,19 @@ int MergeSort(int a[],int l,int r,int count)
 
 int main()
 {
+    //freopen("in.txt","r",stdin);
+    int i,j,k;
     int n;
 
-    cin>>n;
+    scanf("%d",&n);
+
     int a[n];
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
 
-    for(int i=0;i<n;i++)
-        cin>>a[i];
+    int count=MergeSort(a,0,n-1);
 
-    int count=MergeSort(a,0,n-1,0);
-
-    for(int i=0;i<n;i++)
-        cout<<a[i]<<" ";
-
-    cout<<endl;
-    cout<<"inversion:"<<count<<endl;
+    printf("Total inversion:%d\n",count);
 
     return 0;
-
 }
